841c09d203cd55dd736ff15bde22cf9ebd3b0801
MarenViaDrupsteen <90615534+MarenViaDrupsteen@users.noreply.github.com>
2022-03-17 13:03:05 +0100

    search fixed

---
+++ app.js
@@ -0,0 +1,15 @@
+// function getCSSArray()
+// {
+// var links = document.getElementsByTagName("link");
+// var link;
+// for(var i = 0; i < links.length; i++)
+// {
+//     link = links[i];
+//     if(/stylesheet/.test(link.rel))
+//     {
+//         sheets.push(link);
+//     }
+// }
+
+//     return sheets;
+// }
